621391b4d8fad50b6a34757f12887deeb0c7743b
Bruno Andreotti <bandreotti@protonmail.com>
2021-08-17 16:40:59 -0300

    Add script to get spotify data

---
+++ python/spotify.py
@@ -0,0 +1,73 @@
+import csv
+import numpy as np
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+# This reads the spotify API client ID and client secret from the
+# SPOTIPY_CLIENT_ID and SPOTIPY_CLIENT_SECRET environment variables
+sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+
+kpop_artists = [
+    '3Nrfpe0tUJi4K4DXYWgMUX', # BTS
+    '4Kxlr1PRlDKEB0ekOCyHgX', # BIGBANG
+    '41MozSoPIsD1dJM0CLPjZF', # BLACKPINK
+    '3cjEqqelV9zb4BYE3qDQ4O', # EXO
+    '0qlWcS66ohOIi0M8JZwPft', # GFRIEND
+    '0Sadg1vgvaPqGTOjxu0N6c', # Girls' Generation
+    '1z4g3DjTBBZKhvAroFlhOM', # Red Velvet
+    '7nqOGRxlXj7N2JYbgNEjYH', # SEVENTEEN
+    '2hRQKC0gqlZGPrmUKbcchR', # SHINee
+    '6gzXCdfYfFe5XKhPKkYqxV', # SUPER JUNIOR
+    '7n2Ycct7Beij7Dj7meI4X0', # TWICE
+]
+
+# There are the audio features that are numerical and have values between 0.0
+# and 1.0
+features_of_interest = [
+    'acousticness',
+    'danceability',
+    'energy',
+    'instrumentalness',
+    'liveness',
+    'speechiness',
+    'valence',
+]
+
+# Returns the union of the top 10 tracks for an artist for every possible
+# region. That is, if a track is in the top 10 for the given artist in any
+# region, it will be in the returned list.
+def artist_top_tracks(artist_id):
+    all_regions = sp.available_markets()['markets']
+    top_tracks = set()
+    for region in all_regions:
+        for track in sp.artist_top_tracks(artist_id, region)['tracks']:
+            top_tracks.add(track['id'])
+    return list(top_tracks)
+
+# Returns the mean audio features for an artist, in the form of a numpy array.
+# This only returns the features in `features_of_interest`. This is calculated
+# by getting the audio features for every track returned by
+# `artist_top_tracks`, and taking the average.
+def artist_audio_features(artist_id):
+    top_tracks = artist_top_tracks(artist_id)
+    assert len(top_tracks) <= 100
+    mean_features = np.zeros(len(features_of_interest))
+    for track_features in sp.audio_features(top_tracks):
+        for i, key in enumerate(features_of_interest):
+            mean_features[i] += track_features[key]
+    mean_features /= len(top_tracks)
+    return mean_features
+
+# Builds a CSV file with the mean audio features for a series of artists.
+def build_artist_features_csv(artists, out_file, verbose=False):
+    # This function takes around 30 secs per artist
+    with open(out_file, 'w') as out_file:
+        writer = csv.writer(out_file)
+        writer.writerow(['id', 'name'] + features_of_interest)
+        for artist_id in artists:
+            name = sp.artist(artist_id)['name']
+            features = list(artist_audio_features(artist_id))
+            writer.writerow([artist_id, name] + features)
+            print(f'finished {artist_id} ({name})')
+
+build_artist_features_csv(kpop_artists, 'kpop-artist-features.csv', verbose=True)
